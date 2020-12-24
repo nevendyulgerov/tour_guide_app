61fff4931b961343116a9dfdb6ae12657bef501c
Neven Dyulgerov <neven.diulgerov@gmail.com>
2020-12-24 15:33:36 +0200

    Update resources

--- app/src/main/java/com/sofiahardcode/tourguide/NightLifeFragment.java
+++ app/src/main/java/com/sofiahardcode/tourguide/NightLifeFragment.java
@@ -31,6 +31,11 @@ public class NightLifeFragment extends Fragment {
         nightClubs.add(new Place(R.string.club_void, R.string.club_void_description, R.drawable.club_void));
         nightClubs.add(new Place(R.string.club_fargo, R.string.club_fargo_description, R.drawable.club_fargo));
         nightClubs.add(new Place(R.string.quattro_piano_bar, R.string.quattro_piano_bar_description, R.drawable.quattro_piano_bar));
+        nightClubs.add(new Place(R.string.piano_bar_gatsby, R.string.piano_bar_gatsby_description, R.drawable.piano_bar_gatsby));
+        nightClubs.add(new Place(R.string.club_plazma, R.string.club_plazma_description, R.drawable.club_plazma));
+        nightClubs.add(new Place(R.string.rock_bar_download, R.string.rock_bar_download_description, R.drawable.rock_bar_download));
+        nightClubs.add(new Place(R.string.club_pasha, R.string.club_pasha_description, R.drawable.club_pasha));
+        nightClubs.add(new Place(R.string.club_no_sense, R.string.club_no_sense_description, R.drawable.club_no_sense));
 
         ListView listView = rootView.findViewById(R.id.list);
         PlaceAdapter placeAdapter = new PlaceAdapter(getActivity(), nightClubs);
